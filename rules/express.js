3c7faad8b2d469c17f90814536371d75890294bb
alexgo <alex@gounares.com>
2013-11-12 17:23:12 -0800

    lighten up rules

--- rules/express.js
+++ rules/express.js
@@ -2,26 +2,26 @@ module.exports = {
   keys: {
     get: {
       handleArgs: function(trace, clientstate){
-        console.log('got handle args for express get', trace.args['0']);
+        //console.log('got handle args for express get', trace.args['0']);
         //trace.name += trace.args['0'];
         trace.transactionId = trace.args['0'];
-        console.log('trace.name', trace.name);
+        //console.log('trace.name', trace.name);
       }
     },
     emit: {
       handleArgs: function(trace, clientstate){
-        console.log('got handle args for express emit', trace.args['0']);
+        //console.log('got handle args for express emit', trace.args['0']);
         //trace.name += trace.args['0']; 
-        trace.transactionId = trace.args['0'];     
-        console.log('trace.name ', trace.name);       
+        //trace.transactionId = trace.args['0'];     
+        //console.log('trace.name ', trace.name);       
       }
     },
     addListener: {
       handleArgs: function(trace, clientState){
-        console.log('got handle args for express addListener', trace.args['0']);
+        //console.log('got handle args for express addListener', trace.args['0']);
         //trace.name += trace.args['0'];    
-        trace.transactionId = trace.args['0'];   
-        console.log('trace.name ', trace.name);            
+        //trace.transactionId = trace.args['0'];   
+        //console.log('trace.name ', trace.name);            
       }
     }
   }
