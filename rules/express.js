328fa30046f5a5ea26050708dd3a392f84379965
alexgo <alex@gounares.com>
2013-11-12 18:53:07 -0800

    wip

--- rules/express.js
+++ rules/express.js
@@ -5,6 +5,9 @@ module.exports = {
         //console.log('got handle args for express get', trace.args['0']);
         //trace.name += trace.args['0'];
         trace.transactionId = trace.args['0'];
+        trace.originCallbackTop = trace.args['0'];
+      
+        console.log('setting origin top for ', trace.args[0], trace.funInfo.name);
         //console.log('trace.name', trace.name);
       }
     },
