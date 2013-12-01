3423291e77407887b6a2f9a094d47134e236de45
alexgo <alex@gounares.com>
2013-11-30 17:59:39 -0800

    add blacklist rules for jade and url

---
+++ rules/jade.js
@@ -0,0 +1,10 @@
+module.exports = {
+  keys: {
+    filters: {skip: true},
+    utils: {skip: true},
+    Parser: {skip: true},
+    Lexer: {skip: true},
+    nodes: {skip: true},
+    runtime: {skip: true}
+  }
+};
